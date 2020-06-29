e914721ca14e10bac574bd58c243a1486fce548b
Ilan Olkies <ilanolkies@outlook.com>
2020-06-28 23:19:47 -0300

    Add declarations

--- packages/rif-id-ethr-did/src/rskDID.ts
+++ packages/rif-id-ethr-did/src/rskDID.ts
@@ -16,11 +16,11 @@ type RSKDIDFromPrivateKey = (conf?: RSKDIDFromPrivateKeyConf) => (privateKey: st
 export const rskDIDFromPrivateKey: RSKDIDFromPrivateKey = (conf = {}) => (privateKey) => {
   // assign account
   const address = rskAddressFromPrivateKey(privateKey)
-  const confWithAccount = Object.assign({}, conf, { privateKey, address })
+  const confWithAccount = (<any>Object).assign({}, conf, { privateKey, address })
 
   // assign provider
   if (!(conf.provider || conf.web3 || conf.rpcUrl)) {
-    Object.assign(confWithAccount, { rpcUrl: RSK_RPC_URL })
+    (<any>Object).assign(confWithAccount, { rpcUrl: RSK_RPC_URL })
   }
 
   return new EthrDID(confWithAccount)
