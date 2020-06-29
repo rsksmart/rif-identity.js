edce76cdbf5b36e934ea1731a91ba45397663198
Ilan <36084092+ilanolkies@users.noreply.github.com>
2020-06-29 19:24:44 -0300

    core-reducer module (#7)

---
+++ packages/rif-id-core-reducer/src/index.ts
@@ -0,0 +1,8 @@
+import { combineReducers } from '@reduxjs/toolkit'
+import identityProviders from './identityProviders'
+
+const reducer = combineReducers({
+  identityProviders
+})
+
+export default reducer
