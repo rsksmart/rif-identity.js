92606abf4d04c07181b437e7356fbeccc0ec3a61
Ilan Olkies <ilanolkies@outlook.com>
2020-07-29 02:14:47 -0300

    Lint

--- packages/rif-id-comms/src/requestCredential.ts
+++ packages/rif-id-comms/src/requestCredential.ts
@@ -5,4 +5,4 @@ export const hashCredentialRequest = (request: any, token: string) => keccak256(
 
 export const requestCredential = (url: string, request: any) =>
   axios.post(`${url}/requestCredential`, { payload: request })
-    .then(({ data: { token }}) => (() => axios.get(`${url}/?request=${hashCredentialRequest(request, token)}`)))
+    .then(({ data: { token } }) => () => axios.get(`${url}/?request=${hashCredentialRequest(request, token)}`))
--- packages/rif-id-comms/test/requestCredential.test.ts
+++ packages/rif-id-comms/test/requestCredential.test.ts
@@ -2,7 +2,7 @@ import { requestCredential } from '../src/requestCredential'
 
 // TODO: this is not a good test...
 test('request credential', async () => {
-  const request = { did: 'did:ethr:rsk:0x22271517343929682c72F7F3e081E25577364687', metadata: { meta: 'meta' }}
+  const request = { did: 'did:ethr:rsk:0x22271517343929682c72F7F3e081E25577364687', metadata: { meta: 'meta' } }
   const tryReceive = await requestCredential('http://localhost:3000', request)
 
   const data = await tryReceive()
