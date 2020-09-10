57a58ede3c3a6057815954c8b31b427e27659bd1
Ilan Olkies <ilanolkies@outlook.com>
2020-09-10 01:02:55 -0300

    Add declarative details reducer

---
+++ packages/rif-id-core/src/reducers/declarativeDetails.ts
@@ -0,0 +1,44 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+export interface DeclarativeDetails {
+  [name: string]: {
+    type: string
+    value: string
+  }
+}
+
+export interface DeclarativeDetailsState {
+  [did:string]: DeclarativeDetails
+}
+
+interface SetDeclarativeDetailsPayload {
+  did: string
+  declarativeDetails: DeclarativeDetails
+}
+
+const initialState: DeclarativeDetailsState = {}
+
+const declarativeDetailsSlice = createSlice({
+  name: 'declarativeDetails',
+  initialState,
+  reducers: {
+    setDeclarativeDetails(
+      state: DeclarativeDetailsState,
+      { payload: { did, declarativeDetails } }: PayloadAction<SetDeclarativeDetailsPayload>
+    ) {
+      state[did] = Object.assign({}, state[did], declarativeDetails)
+    }
+  }
+})
+
+export const { setDeclarativeDetails } = declarativeDetailsSlice.actions
+
+export const findDeclarativeDetails = (state: DeclarativeDetailsState, did: string) => state[did]
+export const findDeclarativeDetailsMatchingNames = (state: DeclarativeDetailsState, did: string, names: string[]) => {
+  const declarativeDetails = state[did]
+  let result: DeclarativeDetails = {}
+  for(let name of names) result[name] = declarativeDetails[name]
+  return result
+}
+
+export default declarativeDetailsSlice.reducer
