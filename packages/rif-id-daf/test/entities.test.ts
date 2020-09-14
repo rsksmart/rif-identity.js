d560fd41b4d0f900c51549345ca9b3247481e704
Ilan Olkies <ilanolkies@outlook.com>
2020-09-14 00:43:33 -0300

    Extend jest timeout for daf entities test

--- packages/rif-id-daf/test/entities.test.ts
+++ packages/rif-id-daf/test/entities.test.ts
@@ -5,6 +5,8 @@ import { createSqliteConnection, deleteDatabase } from './util'
 
 const database = './rif-id-daf.entities.test.sqlite'
 
+jest.setTimeout(10000)
+
 describe('entities', () => {
   let connection: Connection
 
