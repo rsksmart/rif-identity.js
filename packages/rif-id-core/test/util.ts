08294e791bf8a71dd5ee7113f149d6acba22dc88
Javier Esses <javiesses@hotmail.com>
2020-09-16 01:25:00 -0300

    initialize, add and delete credentials

--- packages/rif-id-core/test/util.ts
+++ packages/rif-id-core/test/util.ts
@@ -108,7 +108,7 @@ export const did = 'did:ethr:rsk:0xf3beac30c498d9e26865f34fcaa57dbb935b0d74'
 export const did2 = 'did:ethr:rsk:0xdcbe93e98e0dcebe677c39a84f5f212b85ba7ef0'
 export const did3 = 'did:ethr:rsk:0xbe935f08e079e7a7c898bcbde5dceba214fe6f2'
 
-export const issueTestCredential = async (subject: string): Promise<Daf.Credential> => {
+export const generateCredential = async (subject: string): Promise<Daf.Credential> => {
   const mnemonic = generateMnemonic(12)
   const database = `test.rif-id-core.credentials.issuer.${new Date().getTime()}.sqlite`
   const agent = await createAgent(database, { mnemonic: mnemonic })
